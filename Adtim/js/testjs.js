7e02fa087fd351400e6c0c95b08bd2e0672db0a6
Timothy Wanume <wanumet@gmail.com>
2024-10-22 17:13:24 +0300

    Create testjs.js

---
+++ Adtim/js/testjs.js
@@ -0,0 +1,7 @@
+// Mobile menu toggle
+const menuToggle = document.querySelector('.menu-toggle');
+const navMenu = document.querySelector('.nav-menu');
+
+menuToggle.addEventListener('click', () => {
+    navMenu.classList.toggle('active');
+});
